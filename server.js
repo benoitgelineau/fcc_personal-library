0c51edce06210a800bb8787c9296200f2ce921f4
Benoît Gelineau <bgelineau.dev@gmail.com>
2018-07-17 18:58:32 +0200

    Add helmet and mongoClient

--- server.js
+++ server.js
@@ -3,6 +3,10 @@
 var express     = require('express');
 var bodyParser  = require('body-parser');
 var cors        = require('cors');
+const helmet    = require('helmet');
+const mongo = require('mongodb').MongoClient;
+// Enable .env variables to be used
+require('dotenv').config();
 
 var apiRoutes         = require('./routes/api.js');
 var fccTestingRoutes  = require('./routes/fcctesting.js');
@@ -13,6 +17,9 @@ var app = express();
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.use(cors({origin: '*'})); //USED FOR FCC TESTING PURPOSES ONLY!
+
+app.use(helmet.noCache());
+app.use(helmet.hidePoweredBy({ setTo: 'PHP 4.2.0' }));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -26,15 +33,27 @@ app.route('/')
 //For FCC testing purposes
 fccTestingRoutes(app);
 
-//Routing for API 
-apiRoutes(app);  
-    
-//404 Not Found Middleware
-app.use(function(req, res, next) {
-  res.status(404)
-    .type('text')
-    .send('Not Found');
-});
+mongo.connect(
+  process.env.DB,
+  (err, db) => {
+    if (err) {
+      console.log('Database error: ' + err);
+    } else {
+      console.log('Successful database connection');
+
+      //Routing for API
+      apiRoutes(app, db);
+
+      //404 Not Found Middleware
+      app.use(function(req, res, next) {
+        res
+          .status(404)
+          .type('text')
+          .send('Not Found');
+      });
+    }
+  }
+);
 
 //Start our server and tests!
 app.listen(process.env.PORT || 3000, function () {
