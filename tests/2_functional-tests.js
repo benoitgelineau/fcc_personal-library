1ee0db4d45f6b20045f743e54c62521fa58e04b8
Benoît Gelineau <bgelineau.dev@gmail.com>
2018-07-18 21:10:17 +0200

    Complete tests

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -41,11 +41,27 @@ suite('Functional Tests', function() {
     suite('POST /api/books with title => create book object/expect book object', function() {
       
       test('Test POST /api/books with title', function(done) {
-        //done();
+        chai.request(server)
+          .post('/api/books')
+          .send({ title: 'Book title test'})
+          .end(function (err, res) {
+            assert.equal(res.status, 201);
+            assert.property(res.body, 'title', 'Books in array should contain title');
+            assert.property(res.body, '_id', 'Books in array should contain _id');
+            assert.equal(res.body.title, 'Book title test');
+            done();
+          });
       });
       
       test('Test POST /api/books with no title given', function(done) {
-        //done();
+        chai.request(server)
+          .post('/api/books')
+          .send({ title: '' })
+          .end(function (err, res) {
+            assert.equal(res.status, 400);
+            assert.equal(res.text, 'Please insert the book title');
+            done();
+          });
       });
       
     });
@@ -54,7 +70,16 @@ suite('Functional Tests', function() {
     suite('GET /api/books => array of books', function(){
       
       test('Test GET /api/books',  function(done){
-        //done();
+        chai.request(server)
+          .get('/api/books')
+          .end(function (err, res) {
+            assert.equal(res.status, 200);
+            assert.isArray(res.body);
+            assert.property(res.body[0], 'commentcount');
+            assert.property(res.body[0], 'title');
+            assert.property(res.body[0], '_id');
+            done();
+          });
       });      
       
     });
@@ -63,11 +88,27 @@ suite('Functional Tests', function() {
     suite('GET /api/books/[id] => book object with [id]', function(){
       
       test('Test GET /api/books/[id] with id not in db',  function(done){
-        //done();
+        chai
+          .request(server)
+          .get('/api/books/5b4f79b55b8aae07511310c5')
+          .end(function(err, res) {
+            assert.equal(res.status, 400);
+            assert.equal(res.text, 'no book exists');
+            done();
+          });
       });
       
       test('Test GET /api/books/[id] with valid id in db',  function(done){
-        //done();
+        chai
+          .request(server)
+          .get('/api/books/5b4f79b55b8aae07511310c6')
+          .end(function (err, res) {
+            assert.equal(res.status, 200);
+            assert.property(res.body, 'comments');
+            assert.property(res.body, 'title');
+            assert.property(res.body, '_id');
+            done();
+          });
       });
       
     });
@@ -76,7 +117,18 @@ suite('Functional Tests', function() {
     suite('POST /api/books/[id] => add comment/expect book object with id', function(){
       
       test('Test POST /api/books/[id] with comment', function(done){
-        //done();
+        chai
+          .request(server)
+          .post('/api/books/5b4f79b55b8aae07511310c6')
+          .send({ comment: 'Test comment' })
+          .end(function(err, res) {
+            assert.equal(res.status, 201);
+            assert.property(res.body, 'comments', 'Books in array should contain commentcount');
+            assert.property(res.body, 'title', 'Books in array should contain title');
+            assert.property(res.body, '_id', 'Books in array should contain _id');
+            assert.include(res.body.comments, 'Test comment');
+            done();
+          });
       });
       
     });
